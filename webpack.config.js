ff948a3360912f1e65d071a0b751021f8f9fba64
Jeff Revesz <jeffrey.revesz@gmail.com>
2017-04-21 18:11:34 -0400

    Resolves #6

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,40 @@
+/* global __dirname */
+
 var path = require("path");
 var webpack = require("webpack");
 
 module.exports = {
-    // create sourcemaps for the bundle
-    devtool: "source-map",
+  // create sourcemaps for the bundle
+  devtool: "source-map",
 
-    // where does the import chain start
-    entry: "./demo/main.js",
+  // where does the import chain start
+  entry: "./demo/main.js",
 
-    // where do the bundle files get written to
-    output: {
-      path: path.resolve(__dirname, "static", "dist"),
-      filename: "bundle.js"
-    },
+  // where do the bundle files get written to
+  output: {
+    path: path.resolve(__dirname, "static", "dist"),
+    filename: "bundle.js"
+  },
 
-    // custom translations and stuff
-    module: {
-      loaders: [
-        // transpile es6 to es5 during build
-        {
-          loader: "babel-loader",
-          test: path.join(__dirname, "demo"),
-          query: {
-            presets: "es2015",
-          },
-        }
-      ]
-    },
+  // custom translations and stuff
+  module: {
+    loaders: [
+      // transpile es6 to es5 during build
+      {
+        test: path.join(__dirname, "demo"),
+      }
+    ]
+  },
 
-    // other plugins
-    plugins: [
-      // Avoid publishing files when compilation fails
-      new webpack.NoErrorsPlugin()
-    ],
+  // other plugins
+  plugins: [
+    // Avoid publishing files when compilation fails
+    new webpack.NoEmitOnErrorsPlugin()
+  ],
 
-    // output formatting
-    stats: {
-      colors: true
-    },
+  // output formatting
+  stats: {
+    colors: true
+  },
 
 };
